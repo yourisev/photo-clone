6b54fcdd004e5318585287b2445b87853a159740
yourisev <sevgama44@gmail.com>
2023-06-17 19:51:22 -0400

    Photo and Photo controller created.

---
+++ src/main/java/com/peam/photoclone/PhotoCloneApplication.java
@@ -0,0 +1,13 @@
+package com.peam.photoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoCloneApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/peam/photoclone/PhotoController.java
@@ -0,0 +1,2 @@
+package com.peam.photoclone;public class PhotoController {
+}
